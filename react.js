49952eeedf6b39dc246493e57030d24865df6efe
Drishti-Dubey <drishtidubey4957@gmail.com>
2025-05-12 16:27:18 +0530

    Add files via upload
    "TYPES OF FUNCTIONS"

---
+++ react.js
@@ -0,0 +1,118 @@
+// IT IS A LIBRARY NOT A FRAMEWORK
+// EXAMPLE JQUERY
+
+import { Component } from "react";
+
+// PROJECT IS INDEPENDENT OF THE LIBRARY
+
+
+// REACT USES REACT VIRTUAL DOM
+
+// ---CREATES A COPY OF THE DOM
+// ---ONLY UPDATES THE NEW COPY
+// ----COPIES ONLY NEW PARTS TO THE PREVIOUS DOM
+
+// INCREASES SPEED
+
+
+//  SINGLE PAGE APPLICATION---SPA--[uses JSX]{JAVASCRIPT XML}
+
+// JSX
+// *********
+
+
+// ALLOWS SYNTAX OF ALL THE LANGUAGES AT ONCE
+
+// BABEL--- CONVERTS THE JSX CODE INTO SEPARATE FILES LIKE HTML,CSS AND JS
+
+
+
+
+// RULES
+// *********
+
+
+
+// 1-- WE CAN RETURN ONLY ONE HTML ELEMENT AT ONCE ,BUT WE CAN WRAP THE ELEMENT INSIDE A PARENT HTML TAG
+
+// 2--we can use jsx inside the for loop
+
+// 3---HMTL ATTRIBUTES LIKE "class" BECOMES "className"
+
+// 4--- HTML TAGS MUST ALWAYS BE CLOSED {ADD /}
+
+// NPX--- TO RENT{TOOL FOR EXECUTING NODE PACKAGES}
+// NPM-- TO BUY
+
+// component--- independent reusable code blocks
+// reusable,easy to read , write and test
+
+// 2 KINDS
+// 1] STATELESS{FUNCTIONAL} 2] STATEFUL{CLASS}
+
+
+
+// EXAMPLES
+
+
+// function Welcome(props)
+// {
+// return <h1>Hello, [props.name]</h1>
+// }
+
+
+// function Example()
+// {
+//     return (<h1>Im a functional component !</h1>);
+// };
+
+
+// const Example=() =>
+// {
+//     return (<h1>Im a functional component !</h1>);
+// };
+
+
+
+// CLASS{STATEFUL COMPONENT} {study examples}
+
+
+// CALLING A COMPONENT
+// <ExampleComponent/>
+
+
+// REACT IS A COMPONENT BASED LIBRARY WHICH DIVIDES THE UI INTO LITTLE REUSABLE PIECES WHICH SEND DATA TO EACH OTHER
+//  UNIDIRECTIONAL {PARENT TO CHILD}
+
+
+// CHILD CAN'T CHANGE PARENT'S DATA
+
+
+// example of class component
+
+// class ParentComponent extends Component
+// {
+//     render()
+//     {
+//         <h1>Im the first component
+//             <ChildComponent/>
+//         </h1>
+//     }
+// }
+// ........
+
+
+// object.props.membername---- to retrieve text
+// ChildComponent someattribute={value}  someattribute={value}
+
+
+// childcomponent text={"text"};
+
+
+// class ParentComponent extends Component
+{
+    render()
+    {
+        
+    }
+}
